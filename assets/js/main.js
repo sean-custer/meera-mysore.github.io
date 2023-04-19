044229625689d0098f032cd084b5f95cf2795f1c
Sean Custer <custer_sean@arsiem.com>
2023-04-19 17:26:42 -0400

    Testing removal of breakpoints JS function for mobile

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,14 +11,14 @@
 		$main = $('#main');
 
 	// Breakpoints.
-		breakpoints({
+		/*breakpoints({
 			xlarge:   [ '1281px',  '1680px' ],
 			large:    [ '981px',   '1280px' ],
 			medium:   [ '737px',   '980px'  ],
 			small:    [ '481px',   '736px'  ],
 			xsmall:   [ '361px',   '480px'  ],
 			xxsmall:  [ null,      '360px'  ]
-		});
+		});*/
 
 	// Play initial animations on page load.
 		$window.on('load', function() {
